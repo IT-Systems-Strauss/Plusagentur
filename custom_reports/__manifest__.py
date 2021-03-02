3a81fc6bf6ae3128a541c4b41df62eea5d5cbccb
Bernd Grossekathoefer <service@its-strauss.de>
2021-03-02 11:21:55 +0100

    xPath Added, Manifest updatet

--- custom_reports/__manifest__.py
+++ custom_reports/__manifest__.py
@@ -27,6 +27,7 @@
         # 'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
+        'reports/sale.report_saleorder_its_custom_document.xml'
     ],
     # only loaded in demonstration mode
     'demo': [
